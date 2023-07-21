6aee639735338c56ae40b93f1939cd67c05b3dc2
aleal76 <aleal76@hotmail.com>
2023-07-21 00:11:46 +0000

    modelos dentro

--- django_project/settings.py
+++ django_project/settings.py
@@ -43,6 +43,7 @@ ALLOWED_HOSTS = ip_addresses()
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
